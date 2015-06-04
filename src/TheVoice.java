2df08af36ce040a60fb50815f8e1554bd8c9a832
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-06-04 20:09:32 +0200

    Dodane teksty

--- src/TheVoice.java
+++ src/TheVoice.java
@@ -1,6 +1,9 @@
 public class TheVoice {
+    public static void parseArgs(String[] args) {
+        System.out.println(args[0]);
+    }
 
     public static void main(String[] args) {
-        // write your code here
+        parseArgs(args);
     }
 }
