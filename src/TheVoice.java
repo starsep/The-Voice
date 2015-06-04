4af5b4385f5a84e71ef2b205200db8538c0f5d3c
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-06-04 23:03:32 +0200

    Zmiany w parsowaniu nazw artystów

--- src/TheVoice.java
+++ src/TheVoice.java
@@ -10,10 +10,13 @@ public class TheVoice {
                 textProcessor = TextProcessorFactory.getTextSource(arg.split("--source-type=")[1]);
         if (textProcessor == null)
             throw new Exception("Nie podany typ żródła");
+        StringBuilder artist_name = new StringBuilder();
+        boolean artistWithSpaces = false;
+
         for (String arg : args) {
-            if (arg.contains("--source="))
+            if (arg.contains("--source=")) {
                 textProcessor.setSource(arg.split("--source=")[1]);
-            else if (arg.contains("--processors=")) {
+            } else if (arg.contains("--processors=")) {
                 String[] processors = arg.split("--processors=")[1].split(",");
                 for (String processor : processors)
                     voice.addProcessor(ProcessorFactory.getProcessor(processor));
@@ -21,29 +24,28 @@ public class TheVoice {
                 String[] filters = arg.split("--filters=")[1].split(File.pathSeparator);
                 for (String filter : filters)
                     voice.addFilter(FilterFactory.getFilter(filter));
-            }
-        }
-        
-        //ZAŁOŻENIE: jeżeli nazwa artysty jest jednowyrazowa to nie jest w cudzysłowie
-        StringBuilder artist_name = new StringBuilder();
-        boolean artistWithSpaces = false;
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            if (!arg.contains("--")) {
+            } else if (!arg.contains("--")) {
                 if (arg.charAt(0) == '“') {
-                    artistWithSpaces = true;
-                    artist_name.append(arg.substring(1) + " ");
+                    if (arg.charAt(arg.length() - 1) == '”') {
+                        voice.addArtist(new Artist(arg.substring(1, arg.length() - 1)));
+                    } else {
+                        artistWithSpaces = true;
+                        artist_name.append(arg.substring(1));
+                        artist_name.append(" ");
+                    }
                 } else if (arg.charAt(arg.length() - 1) == '”') {
                     artistWithSpaces = false;
                     artist_name.append(arg.substring(0, arg.length() - 1));
                     voice.addArtist(new Artist(artist_name.toString()));
                     artist_name.setLength(0);
-                } else if (artistWithSpaces)
-                    artist_name.append(arg + " ");
-                else //if(!artistWithSpaces)
+                } else if (artistWithSpaces) {
+                    artist_name.append(arg);
+                    artist_name.append(" ");
+                } else //if(!artistWithSpaces)
                     voice.addArtist(new Artist(arg));
             }
         }
+
         voice.setTextProcessor(textProcessor);
     }
 
